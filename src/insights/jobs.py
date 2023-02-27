fd5b1094a16112bd6973b6720b699aaedbc6c6dd
p4n1k0 <gabre.tarick@gmail.com>
2023-02-27 12:59:34 -0300

    funcao q abre arqv csv e retorna dados em formato de lista de dicionario

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
@@ -16,7 +17,8 @@ def read(path: str) -> List[Dict]:
     list
         List of rows as dicts
     """
-    raise NotImplementedError
+    with open(path) as file:
+        return [item for item in csv.DictReader(file)]
 
 
 def get_unique_job_types(path: str) -> List[str]:
