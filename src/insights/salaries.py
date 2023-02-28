561f23993b9685906ffb29c04473c7b19869ef85
p4n1k0 <gabre.tarick@gmail.com>
2023-02-28 15:06:04 -0300

    ignora empregos d valores invalidos d min ou mx salary e retorna lista com tds empregos ond o salario estiver entre as colun min e max salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -83,4 +83,11 @@ def filter_by_salary_range(
     list
         Jobs whose salary range contains `salary`
     """
-    raise NotImplementedError
+    filtered = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered.append(job)
+        except ValueError:
+            continue
+    return filtered
